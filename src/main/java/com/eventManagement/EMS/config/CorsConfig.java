a32bde485dbbe7a59d7d2a6c1429f829f0df1866
Breshly Abanid <breshlyabanid@gmail.com>
2024-07-08 16:49:47 +0800

    Added CorsConfig.java

---
+++ src/main/java/com/eventManagement/EMS/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.eventManagement.EMS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer(){
+                @Override
+                public void addCorsMappings(CorsRegistry registry){
+                    registry.addMapping("/**")
+                            .allowedOrigins("http://localhost:5173")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+        };
+    }
+}
